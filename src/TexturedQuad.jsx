38f269844a90965c812b18655467c76e163a9243
Younes <younes.elberkennou@aalto.fi>
2024-10-13 08:18:41 +0300

    Configured github site, removed Drei dependence

--- src/TexturedQuad.jsx
+++ src/TexturedQuad.jsx
@@ -1,16 +1,16 @@
-import {Plane} from "@react-three/drei";
 import React from "react";
 
 function TexturedQuad(texture, transparent_p) {
-    return (
-        <Plane scale={[2, 2, 1]}>
-            <meshBasicMaterial
-                attach="material"
-                transparent={transparent_p}
-                map={texture.texture}
-            />
-        </Plane>
-    );
+  return (
+    <mesh>
+      <planeGeometry args={[2, 2, 1]} />
+      <meshBasicMaterial
+        attach="material"
+        transparent={transparent_p}
+        map={texture.texture}
+      />
+    </mesh>
+  );
 }
 
 export default TexturedQuad;
